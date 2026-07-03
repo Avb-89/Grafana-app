2887cd2ca2fb604086aef9d3331e549bcf787e78
Sitis <darkarhont@icloud.com>
2026-07-03 09:56:13 +0300

    Initial Commit

---
+++ Grafana/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grafana
+//
+//  Created by SITIS on 7/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grafana/GrafanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrafanaApp.swift
+//  Grafana
+//
+//  Created by SITIS on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GrafanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
